0ad48ef6fa82cc7e106dfe6fff673ed2c79500a3
sztanyikz2 <sztanyik.zoltan20k@egressy.info>
2025-12-01 11:26:43 +0100

    alap DTOk és adatbázis befejezése

--- vizsgaController/Persistence/VizsgaDbContext.cs
+++ vizsgaController/Persistence/VizsgaDbContext.cs
@@ -4,11 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace vizsgaController.Persistence
 {
-    public class VizsgaDbContext:DbContext
+    public class VizsgaDbContext : DbContext
     {
         public DbSet<User> users { get; set; }
         public DbSet<Admin> admin { get; set; }
-        public VizsgaDbContext(DbContextOptions<VizsgaDbContext> options): base(options) { }
+        public VizsgaDbContext(DbContextOptions<VizsgaDbContext> options) : base(options) { }
     }
 
     public class User
@@ -28,5 +28,75 @@ namespace vizsgaController.Persistence
         public string adminname { get; set; }
         public string adminemail { get; set; }
         public string adminpassword { get; set; }
+    }
+
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int postID { get; set; }
+        public int userID { get; set; }
+        public int categoryID { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string content { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime deleted_at { get; set; }
+
+    }
+
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int categoryID { get; set; }
+        public string categoryname { get; set; }
+        public string categorydescription { get; set; }
+    }
+
+    public class Report
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reportID { get; set; }
+        public int postID { get; set; }
+        public int userID { get; set; }
+        [Required]
+        public string reportreason { get; set; }
+        public DateTime reportcreated_at { get; set; }
+        public string reportstatus { get; set; }
+
+    }
+
+    public class Upvote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int upvoteID { get; set; }
+        public int postID { get; set; }
+        public int userID { get; set; }
+        public DateTime upvotecreated_at { get; set; }
+
+    }
+
+    public class Downvote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int downvoteID { get; set; }
+        public int postID { get; set; }
+        public int userID { get; set; }
+        public DateTime downvotecreated_at { get; set; }
+
+    }
+    public class Comment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int commentID { get; set; }
+        public int postID { get; set; }
+        public string commentcoontent { get; set; }
+        public DateTime commentcreated_at { get; set; }
     }
 }
