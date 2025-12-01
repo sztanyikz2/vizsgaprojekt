e0739591bd9f082a3c21ff32377306fc717661b4
sztanyikz2 <sztanyik.zoltan20k@egressy.info>
2025-12-01 10:35:20 +0100

    adatbázis alap, model, loginmodel, controller,logincontroller,dtok

---
+++ vizsgaController/Dtos/AdminDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace vizsgaController.Dtos
+{
+    public class AdminDTO
+    {
+        public int adminID { get; set; }
+        public string adminname { get; set; }
+        public string adminemail { get; set; }
+        public string adminpassword { get; set; }
+    }
+}
---
+++ vizsgaController/Dtos/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace vizsgaController.Dtos
+{
+    public class UserDTO
+    {
+        public int userID { get; set; }
+        public string username { get; set; }
+        public string useremail { get; set; }
+        public string userpassword { get; set; }
+    }
+}
