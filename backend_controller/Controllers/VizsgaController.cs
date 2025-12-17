c40668b0dd7637c5b87aec38a5070ec1faff2c81
sztanyikz2 <sztanyik.zoltan20k@egressy.info>
2025-12-17 11:15:21 +0100

    függvények és a hozzájuk tartozó controllerek megírása, kettő interface létrehozása

--- backend_controller/Controllers/VizsgaController.cs
+++ backend_controller/Controllers/VizsgaController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using vizsgaController.Model;
 
 namespace vizsgaController.Controllers
 {
@@ -7,5 +9,137 @@ namespace vizsgaController.Controllers
     [ApiController]
     public class VizsgaController : ControllerBase
     {
+
+        private readonly IVizsgaModel _model;
+        public VizsgaController(IVizsgaModel model)
+        {
+            _model = model;
+        }
+        [HttpGet("/searchuser")]
+        public IActionResult GetUserNameBySearch([FromQuery] string name)
+        {
+            try
+            {
+                var users = _model.GetUserNamesBySearch(name);
+                return Ok(users);
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+        [HttpGet("searchpost")]
+        public IActionResult GetPostBySeatch([FromQuery] string title)
+        {
+            try
+            {
+                var posts = _model.GetPostsBySearch(title);
+                return Ok(posts);
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpDelete("deletepost")]
+        public IActionResult DeletePost([FromQuery] int id)
+        {
+            try
+            {
+                _model.DeletePost(id);
+                return Ok("Post deleted successfully");
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpDelete("moderatecomments")]
+        public IActionResult ModerateComments([FromQuery] int id)
+        {
+            try
+            {
+                _model.ModerateComments(id);
+                return Ok("Comment moderated successfully");
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+        [Authorize(Roles ="User")]
+        [HttpDelete("deleteownpost")]
+        public IActionResult DeleteOwnPost([FromQuery] int postid, int userid)
+        {
+            try
+            {
+                _model.DeleteOwnPost(postid,userid);
+                return Ok("Saját poszt sikeresen törölve");
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+        [HttpPost("favouriteposts")]
+        public IActionResult FavouritePosts([FromQuery] int postID, int userID)
+        {
+            try
+            {
+                _model.FavouritePost(postID, userID);
+                return Ok("Poszt sikeresen hozzáadva a kedvencekhez");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpDelete("manageusers")]
+        public IActionResult ManageUsers([FromQuery] int id)
+        {
+            try
+            {
+                _model.DeleteUsers(id);
+                return Ok("User managed successfully");
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Hiba történt");
+            }
+        }
+
+        [Authorize(Roles ="Admin")]
+
     }
 }
--- backend_controller/Model/VizsgaModel.cs
+++ backend_controller/Model/VizsgaModel.cs
@@ -1,49 +1,131 @@
-﻿using vizsgaController.Dtos;
+﻿using Microsoft.EntityFrameworkCore;
+using vizsgaController.Dtos;
 using vizsgaController.Persistence;
 
 namespace vizsgaController.Model
 {
-    public class VizsgaModel
+    public class VizsgaModel : IVizsgaModel
     {
         private readonly VizsgaDbContext _context;
         public VizsgaModel(VizsgaDbContext context)
         {
             _context = context;
         }
-
         public IEnumerable<UserDTO> GetUserNamesBySearch(string name)
         {
             if (name != null)
             {
-                return _context.users.Where(x => x.username.ToLower() == name.ToLower()).Select(x => new UserDTO
+                return _context.Users.Where(x => x.Username.ToLower() == name.ToLower()).Select(x => new UserDTO
                 {
-                    username = x.username,
-                    useremail = x.useremail,
-                    userpassword = x.userpassword,
+                    username = x.Username,
+                    useremail = x.Useremail,
+                    userpassword = x.Userpassword,
                 });
 
 
             }
             throw new InvalidDataException("Töltsd ki a keresőmezőt, pretty please");
         }
-
-
         public IEnumerable<PostDTO> GetPostsBySearch(string title)
         {
             if (title != null)
             {
-                return _context.posts.Where(x => x.title.ToLower().Contains(title.ToLower())).Select(x => new PostDTO
+                return _context.Posts.Where(x => x.Title.ToLower().Contains(title.ToLower())).Select(x => new PostDTO
                 {
-                    title = x.title,
-                    content = x.content,
-                    created_at = x.created_at,
-                    deleted_at = x.deleted_at,
+                    title = x.Title,
+                    content = x.Content,
+                    created_at = x.Created_at,
+                    deleted_at = x.Deleted_at,
                 });
             }
             throw new InvalidDataException("Töltsd ki a keresőmezőt, pretty please");
         }
+        public void DeletePost(int id)
+        {
+            using var trx = _context.Database.BeginTransaction();
+            {
+                _context.Remove(_context.Posts.Where(x => x.PostID == id).First());
+                _context.SaveChanges();
+                trx.Commit();
+            }
+        }
+        public void ModerateComments(int id)
+        {
 
+            using var trx = _context.Database.BeginTransaction();
+            {
+                _context.Comments.Remove(_context.Comments.Where(x => x.CommentID == id).FirstOrDefault());
+                _context.SaveChanges();
+                trx.Commit();
+            }
+        }
+        public void DeleteOwnPost(int postid, int userid)
+        {
 
+            using var trx = _context.Database.BeginTransaction();
+            {
+                _context.Posts.Remove(_context.Posts.Where(x => x.PostID == postid && x.UserID == userid).FirstOrDefault()); ///usert valahogyan használni kellene, összekötni
+                _context.Comments.Remove(_context.Comments.Where(x => x.PostID == postid).FirstOrDefault());
+                _context.SaveChanges();
+                trx.Commit();
+            }
+        }
+        public void FavouritePost(int postId, int userId)
+        {
+            using var trx = _context.Database.BeginTransaction();
+            {
+                var post = _context.Posts.Where(x => x.PostID == postId && x.UserID == userId).FirstOrDefault();
+                if (post != null)
+                {
+                    post.Favourite = true;
+                    _context.SaveChanges();
+                    trx.Commit();
+                }
+                else
+                {
+                    throw new InvalidDataException("Post or User not found");
+                }
+            }
 
+        }
+        public void DeleteUsers(int id)
+        {
+            using var trx = _context.Database.BeginTransaction();
+            {
+                _context.Users.Remove(_context.Users.Where(x => x.UserID == id).FirstOrDefault());
+                _context.SaveChanges();
+                trx.Commit();
+            }
+        }
+        public void ModifyUsers(int id, string name)
+        {
+            using var trx = _context.Database.BeginTransaction();
+            {
+                var user = _context.Users.Where(x => x.UserID == id).FirstOrDefault();
+                if (user != null)
+                {
+                    user.Username = name ;
+                    _context.SaveChanges();
+                    trx.Commit();
+                }
+                else
+                {
+                    throw new InvalidDataException("User not found");
+                }
+            }
+        }
+        public void VoteOnPost()
+        {
+        }
+        public void CommentOnPost()
+        {
+        }
+       
+        public void PostContent()
+        {
+        }
+        public void ManageSiteSettings()
+        {
+        }
     }
 }
