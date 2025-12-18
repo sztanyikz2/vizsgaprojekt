8e26322564524de9768aef6768b7ebf612a22431
sztanyikz2 <sztanyik.zoltan20k@egressy.info>
2025-12-18 11:37:13 +0100

    img alap megcsinálása

---
+++ backend_controller/Controllers/ImageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using vizsgaController.Model;
+
+namespace vizsgaController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController:ControllerBase
+    {
+        private readonly IImageModel _model;
+        public ImageController(IImageModel model)
+        {
+            _model = model;
+        }
+
+        [HttpPost("/Upload")]
+        public async Task<ActionResult> Upload([FromForm] int userId, [FromForm] IFormFile file)
+        {
+            try
+            {
+                int id = await _model.SaveImageAsync(userId, file);
+                return Ok(new { imageId = id });
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("/Image/{id}")]
+        public IActionResult GetImage(int id)
+        {
+            try
+            {
+                var img = _model.GetImage(id);
+                ///return File(img.Content, img.ContentType);
+                return null;
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ backend_controller/Model/ImageModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vizsgaController.Persistence;
+
+namespace vizsgaController.Model
+{
+    public class ImageModel
+    {
+        private readonly NewsDbContext _context;
+        public ImageModel(NewsDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> SaveImageAsync(int postid, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                //something exception;
+            }
+            using var ms = new MemoryStream();
+            await file.CopyToAsync(ms);
+
+            var img = new Image
+            {
+                PostId = postid,
+                ImageContent = ms.ToArray(),
+                ContentType = file.ContentType
+            };
+
+            _context.Images.Add(img);
+            await _context.SaveChangesAsync();
+
+            return img.ImageId;
+        }
+
+        public byte[] GetImage(int id)
+        {
+            var image = _context.Find<Image>([id]);
+            if (image == null)
+            {
+                throw new KeyNotFoundException();
+            }
+            return image.ImageContent;
+        }
+
+    }
+}
