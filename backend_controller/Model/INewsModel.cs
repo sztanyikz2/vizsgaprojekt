acb2e33d40291c7b343fc010399c042e7c3fc883
Smylann <pelyvas.milan20k@egressy.info>
2026-01-07 09:51:08 +0100

    interface fixed

--- backend_controller/Model/INewsModel.cs
+++ backend_controller/Model/INewsModel.cs
@@ -4,17 +4,31 @@ namespace vizsgaController.Model
 {
     public interface INewsModel
     {
+        //search
         public IEnumerable<UserDTO> GetUserNamesBySearch(string name);
         public IEnumerable<PostDTO> GetPostsBySearch(string title);
-        public void DeletePost(int id);
+        //users
         public void DeleteUsers(int id);
         public void ModifyUsers(int id, string name);
-        public void ModerateComments(int id);
-        public void ManageSiteSettings();
-        public void PostContent();
+        //posts
+        public void CreatePost(PostDTO source);
+        public void DeletePost(int id);
         public void DeleteOwnPost(int postid, int userid);
-        public void CommentOnPost();
-        public void VoteOnPost();
         public void FavouritePost(int postId, int userId);
+        public void UnfavouritePost(int postId, int userId);
+        public void UpVoteOnPost(int postId, int userid);
+        public void DownVoteOnPost(int postId, int userid);
+        //coment
+        public void CommentOnPost(CommentDTO source);
+        public void DeleteComments(int id);
+        //categories
+        public void CreateCategory(CategoryDTO source);
+        public void DeleteCategory(int id);
+        //reports
+        public void CreateReport(ReportDTO source);
+        //smth
+        public void ManageSiteSettings();
+        
+
     }
 }
