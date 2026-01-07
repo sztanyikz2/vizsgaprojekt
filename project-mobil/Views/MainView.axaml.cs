85ad48ed07d51e7fbe2f09d707e8b92c6cb72eef
Csabi <csabi@csabi.zip>
2026-01-07 10:47:03 +0100

    mobile

---
+++ project-mobil/Views/MainView.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace project_mobil.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void OpenPane_Click(object? sender, RoutedEventArgs e)
+    {
+        MainSplitView.IsPaneOpen = true;
+    }
+
+    private void ClosePane_Click(object? sender, RoutedEventArgs e)
+    {
+        MainSplitView.IsPaneOpen = false;
+    }
+}
