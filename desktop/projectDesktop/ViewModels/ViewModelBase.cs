c5fb530dc706d258d3a23e6e5b6f3c26503e2812
FCsab <csabi@csabi.zip>
2026-02-05 10:14:13 +0100

    avalonia start

---
+++ desktop/projectDesktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace projectDesktop.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ desktop/projectDesktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace projectDesktop.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
