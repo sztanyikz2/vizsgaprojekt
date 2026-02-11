1c32684b452d92a6658244d2357e4020321cef61
Pelyvás Milán <161708891+Smylann@users.noreply.github.com>
2026-02-11 20:53:40 +0100

    Delete project-mobil directory

--- project-mobil/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace project_mobil.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- project-mobil/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace project_mobil.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
